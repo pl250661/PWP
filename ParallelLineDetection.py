a0e9b6a11f767e6416d805be8e024932acfd4491
Thomas Zanatta <149692642+pl250661@users.noreply.github.com>
2025-01-04 19:35:17 -0500

    final entry

--- ParallelLineDetection.py
+++ ParallelLineDetection.py
@@ -1,3 +1,5 @@
+# Thomas Zanatta
+
 import cv2
 import numpy as np
 
@@ -6,15 +8,16 @@ video = cv2.VideoCapture(0)
 def find_lines(vid):
    gray = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
 
-   blur = cv2.GaussianBlur(gray, (7, 7), 0)
+   blur = cv2.GaussianBlur(gray, (1, 1), 0)
 
-   edges = cv2.Canny(blur, 200, 500, apertureSize=5)
+   edges = cv2.Canny(blur, 500, 700, apertureSize=5)
 
    # https: //docs.opencv.org/4.x/d3/de6/tutorial_js_houghlines.html
-   lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 400, minLineLength=100, maxLineGap=500)
+   lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 300, minLineLength=100, maxLineGap=500)
 
    if lines is not None:
-       threshold = 10
+       angle_threshold = 10
+       intercept_threshold = 300
        parallel_lines = []
        for line1 in lines:
            for line2 in lines:
@@ -23,12 +26,14 @@ def find_lines(vid):
                    x3, y3, x4, y4 = line2[0]
 
                    slope1 = (y2 - y1) / (x2 - x1 + 0.00001)
+                   intercept1 = y1 - slope1 * x1
                    slope2 = (y4 - y3) / (x4 - x3 + 0.00001)
+                   intercept2 = y3 - slope2 * x3
 
                    angle1 = np.arctan(slope1) * 180 / np.pi
                    angle2 = np.arctan(slope2) * 180 / np.pi
 
-                   if abs(angle1-angle2) < threshold:
+                   if abs(angle1-angle2) < angle_threshold and abs(intercept1 - intercept2) > intercept_threshold:
                        parallel_lines.append((line1, line2))
 
        for line1, line2 in parallel_lines:
@@ -60,3 +65,4 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
+ 
